6cb9eea778e62dd835b7d9842eead97021d531d7
JakeMullins <81662558+JakeMullins@users.noreply.github.com>
2021-07-29 15:33:58 -0700

    screw this i just deleted it

--- models/exoplanet.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-
-var exoSchema = new Schema({
-    pl_name: { type: String, require: true }
-});
-
-module.exports = mongoose.model('exoplanets')
