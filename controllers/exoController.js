5aa8d6e82a1faf1e2e12dd7bc0fe76b36366ba69
JakeMullins <81662558+JakeMullins@users.noreply.github.com>
2021-08-13 16:39:30 -0700

    Pretty pretty princess stuff

--- controllers/exoController.js
+++ controllers/exoController.js
@@ -7,6 +7,7 @@ exports.index = function(req, res) {
         console.log(currentCount);
         // Modify this to change counter page
         res.render("counter", {
+            title: "Realtime Exoplanet Counter",
             totalCount: currentCount,
             // How many have been discovered since site went live
             sinceCount: currentCount - 4438,
